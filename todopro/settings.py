d3518ffb8ed578ca977364b451a6f52b8f3985aa
vizbeth <vizbethdigital@gmail.com>
2020-04-22 08:53:14 +0000

    settings updated

--- todopro/settings.py
+++ todopro/settings.py
@@ -5,7 +5,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-env = environ.Env(SECRET_KEY = str)
+env = environ.Env(SECRET_KEY = str,)
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
